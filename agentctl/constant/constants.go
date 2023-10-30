2c0a2ecc0ff6d20241337684c7870cb3945ceab6
baojingh <baojingh@163.com>
2023-10-30 21:26:17 +0800

    update

---
+++ agentctl/constant/constants.go
@@ -0,0 +1,7 @@
+package constant
+
+var (
+	DATE_FORMAT       string = "2006-01-22 11:22:33"
+	DEFAULT_LOG_PATH  string = "/var/log/agent-server"
+	DEFAULT_CERT_PATH string = "/opt/agentctl/cert"
+)
